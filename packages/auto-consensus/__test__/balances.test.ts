eb16b096e4acc8cd359e75da3a57088af634893c
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-06-03 12:27:34 -0400

    add basic balances tests

---
+++ packages/auto-consensus/__test__/balances.test.ts
@@ -0,0 +1,21 @@
+import { activate, disconnect } from '../src/api'
+import { totalIssuance } from '../src/balances'
+
+describe('Verify balances functions', () => {
+  beforeAll(async () => {
+    await activate()
+  })
+
+  afterAll(async () => {
+    await disconnect()
+  })
+
+  test('Check totalIssuance return a number greater than zero', async () => {
+    // totalIssuance is an async function that returns a hex number as a string
+    const rawIssuance = await totalIssuance()
+    // Convert the hex number to a BigInt
+    const issuance = BigInt(rawIssuance.toString())
+    // Check if the issuance is greater than zero
+    expect(issuance).toBeGreaterThan(BigInt(0))
+  })
+})
