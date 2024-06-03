dc734aab5607ec2d669708626f9c43441cac8a58
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-06-03 17:18:56 -0400

    move some functions in auto-utils and improve monorepo with yarn berry

---
+++ packages/auto-utils/src/api.ts
@@ -0,0 +1,49 @@
+import { ApiPromise, WsProvider } from '@polkadot/api'
+import { getNetworkDomainRpcUrls, getNetworkRpcUrls } from './network'
+import type { DomainInput, NetworkInput } from './types/types'
+
+let provider: WsProvider | null = null
+let apiInstance: ApiPromise | null = null
+
+let domainProvider: WsProvider | null = null
+let apiDomainInstance: ApiPromise | null = null
+
+export const activate = async (input: NetworkInput) => {
+  // Get the first rpc urls for the network
+  const rpcUrl = getNetworkRpcUrls(input)
+  // Create the provider
+  provider = new WsProvider(rpcUrl[0])
+  // Create the API instance
+  apiInstance = await ApiPromise.create({ provider })
+
+  return apiInstance
+}
+
+export const activateDomain = async (input: DomainInput) => {
+  // Get the first rpc urls for the network
+  const rpcUrl = getNetworkDomainRpcUrls(input)
+  // Create the provider
+  domainProvider = new WsProvider(rpcUrl[0])
+  // Create the API instance
+  apiDomainInstance = await ApiPromise.create({ provider: domainProvider })
+
+  return apiDomainInstance
+}
+
+export const disconnect = async () => {
+  // Disconnect the API instance and the provider if they exist
+  if (apiInstance) {
+    await apiInstance.disconnect()
+    apiInstance = null
+    provider = null
+  }
+}
+
+export const disconnectDomain = async () => {
+  // Disconnect the API instance and the provider if they exist
+  if (apiDomainInstance) {
+    await apiDomainInstance.disconnect()
+    apiDomainInstance = null
+    domainProvider = null
+  }
+}
---
+++ packages/auto-utils/src/constants/network.ts
@@ -0,0 +1,61 @@
+import type { Network } from '../types/types'
+
+export const networks: Network[] = [
+  {
+    id: 'autonomys-gemini-3h',
+    name: 'Autonomys Tesnet - Gemini 3H',
+    rpcUrls: [
+      'wss://rpc-0.gemini-3h.subspace.network/ws',
+      'wss://rpc-1.gemini-3h.subspace.network/ws',
+    ],
+    explorer: [
+      {
+        name: 'Astral',
+        url: 'https://explorer.subspace.network/gemini-3h/consensus/',
+      },
+      {
+        name: 'Subscan',
+        url: 'https://subspace.subscan.io/',
+      },
+    ],
+    domains: [
+      {
+        id: 'auto-id', // Placeholder
+        name: 'Autonomys - Auto-ID',
+        rpcUrls: ['wss://rpc.auto-id.subspace.network/ws'],
+      },
+      {
+        id: 'auto-evm', // Placeholder
+        name: 'Autonomys - Auto-EVM (Nova)',
+        rpcUrls: ['https://nova-0.gemini-3h.subspace.network/ws'],
+      },
+    ],
+    isTestnet: true,
+  },
+  {
+    id: 'autonomys-localhost',
+    name: 'Autonomys - Localhost',
+    rpcUrls: ['ws://127.0.0.1:9944/ws'],
+    explorer: [
+      {
+        name: 'Astral',
+        url: 'https://explorer.subspace.network/localhost/consensus/',
+      },
+    ],
+    domains: [
+      {
+        id: 'auto-id', // Placeholder
+        name: 'Autonomys - Auto-ID',
+        rpcUrls: ['ws://127.0.0.1:9945/ws'],
+      },
+      {
+        id: 'auto-evm', // Placeholder
+        name: 'Autonomys - Auto-EVM (Nova)',
+        rpcUrls: ['https:///127.0.0.1:9946/ws'],
+      },
+    ],
+    isTestnet: true,
+  },
+]
+
+export const defaultNetwork = networks[0]
