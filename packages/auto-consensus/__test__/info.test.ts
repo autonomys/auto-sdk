622826eda5a270b35af501acbb38a7b58e7774f7
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-06-03 12:29:51 -0400

    add basic info test

---
+++ packages/auto-consensus/__test__/info.test.ts
@@ -0,0 +1,21 @@
+import { activate, disconnect } from '../src/api'
+import { currentTimestamp } from '../src/info'
+
+describe('Verify info functions', () => {
+  beforeAll(async () => {
+    await activate()
+  })
+
+  afterAll(async () => {
+    await disconnect()
+  })
+
+  test('Check timestamp return a number greater than zero', async () => {
+    // totalIssuance is an async function that returns a hex number as a string
+    const rawTimestamp = await currentTimestamp()
+    // Convert the hex number to a BigInt
+    const timestamp = BigInt(rawTimestamp.toString())
+    // Check if the issuance is greater than zero
+    expect(timestamp).toBeGreaterThan(BigInt(0))
+  })
+})
