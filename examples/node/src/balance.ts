c63ee6c07137ed4601d0ecc0840f7097f0d008e8
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-06-19 12:03:41 -0400

    add few scripts and .env

---
+++ examples/node/src/balance.ts
@@ -0,0 +1,43 @@
+import { address, balance } from '@autonomys/auto-consensus'
+import { setup } from './utils/setup'
+
+const main = async () => {
+  const { api, alice, bob } = await setup()
+
+  // Alice's Addresses and Balance
+  const aliceAddress = address(alice[0].address)
+  console.log('\x1b[32m%s\x1b[0m', 'Alice Clean Address:', aliceAddress)
+  const aliceBalance = await balance(api, aliceAddress)
+  console.log(
+    '\x1b[36m%s\x1b[0m',
+    'Alice Free Balance:',
+    aliceBalance.free.toString(),
+    '\x1b[36m',
+    'ATC',
+    '\x1b[0m\n',
+  )
+
+  // Bob's Addresses and Balance
+  const bobAddress = address(bob[0].address)
+  console.log('\x1b[32m%s\x1b[0m', 'Bob Clean Address:', bobAddress)
+  const bobBalance = await balance(api, bobAddress)
+  console.log(
+    '\x1b[36m%s\x1b[0m',
+    'Bob Free Balance:',
+    bobBalance.free.toString(),
+    '\x1b[36m',
+    'ATC',
+    '\x1b[0m\n',
+    '\n',
+  )
+}
+
+main()
+  .then(() => {
+    console.log('\x1b[34m%s\x1b[0m', 'Script executed successfully')
+    process.exit(0)
+  })
+  .catch((e) => {
+    console.error('\x1b[31m%s\x1b[0m', 'Error with script:', e)
+    process.exit(1)
+  })
