87446faa80dd1338c535e2a5bb8d04a908d64717
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-07-03 10:24:13 -0400

    switch for dynamic import

--- examples/node/src/cli.ts
+++ examples/node/src/cli.ts
@@ -1,56 +1,88 @@
 // src/cli.ts
 import { Command } from 'commander'
-import { addressFunction } from './address'
-import { balanceFunction } from './balance'
-import { deregisterOperatorFunction } from './deregister-operator'
-import { nominateOperatorFunction } from './nominate-operator'
-import { operatorsFunction } from './operators'
-import { registerOperatorFunction } from './register-operator'
-import { transferFunction } from './transfer'
-import { unlockFundsFunction } from './unlock-funds'
-import { unlockNominatorFunction } from './unlock-nominator'
-import { withdrawStakeFunction } from './withdraw-stake'
 
 const program = new Command()
 
 program.name('auto-cli').description('CLI for Autonomys functions').version('0.1.0')
 
-program.command('address').description('Run the address function').action(addressFunction)
+program
+  .command('address')
+  .description('Run the address function')
+  .action(async () => {
+    const { addressFunction } = await import('./address')
+    addressFunction()
+  })
 
-program.command('balance').description('Run the balance function').action(balanceFunction)
+program
+  .command('balance')
+  .description('Run the balance function')
+  .action(async () => {
+    const { balanceFunction } = await import('./balance')
+    balanceFunction()
+  })
 
-program.command('transfer').description('Run the transfer function').action(transferFunction)
+program
+  .command('transfer')
+  .description('Run the transfer function')
+  .action(async () => {
+    const { transferFunction } = await import('./transfer')
+    transferFunction()
+  })
 
-program.command('operators').description('Run the operators function').action(operatorsFunction)
+program
+  .command('operators')
+  .description('Run the operators function')
+  .action(async () => {
+    const { operatorsFunction } = await import('./operators')
+    operatorsFunction()
+  })
 
 program
   .command('register-operator')
   .description('Run the register operator function')
-  .action(registerOperatorFunction)
+  .action(async () => {
+    const { registerOperatorFunction } = await import('./register-operator')
+    registerOperatorFunction()
+  })
 
 program
   .command('nominate-operator')
   .description('Run the nominate operator function')
-  .action(nominateOperatorFunction)
+  .action(async () => {
+    const { nominateOperatorFunction } = await import('./nominate-operator')
+    nominateOperatorFunction()
+  })
 
 program
   .command('withdraw-stake')
   .description('Run the withdraw stake function')
-  .action(withdrawStakeFunction)
+  .action(async () => {
+    const { withdrawStakeFunction } = await import('./withdraw-stake')
+    withdrawStakeFunction()
+  })
 
 program
   .command('deregister-operator')
   .description('Run the deregister operator function')
-  .action(deregisterOperatorFunction)
+  .action(async () => {
+    const { deregisterOperatorFunction } = await import('./deregister-operator')
+    deregisterOperatorFunction()
+  })
 
 program
   .command('unlock-funds')
   .description('Run the unlock funds function')
-  .action(unlockFundsFunction)
+  .action(async () => {
+    const { unlockFundsFunction } = await import('./unlock-funds')
+    unlockFundsFunction()
+  })
 
 program
   .command('unlock-nominator')
   .description('Run the unlock nominator function')
-  .action(unlockNominatorFunction)
+  .action(async () => {
+    const { unlockNominatorFunction } = await import('./unlock-nominator')
+    unlockNominatorFunction()
+  })
 
 program.parse(process.argv)
