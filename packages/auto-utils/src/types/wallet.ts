71ce1b66cff775fd07788f9ce56b9763692d3a09
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-07-04 08:05:07 -0400

    Add function to generate seed, expose keyring and more utility #58

--- packages/auto-utils/src/types/wallet.ts
+++ packages/auto-utils/src/types/wallet.ts
@@ -1,5 +1,11 @@
 // file: src/types/wallet.ts
 
+import type { ApiPromise } from '@polkadot/api'
+import type { Signer, SignerResult } from '@polkadot/api/types'
+import type { InjectedAccountWithMeta } from '@polkadot/extension-inject/types'
+import type { KeyringPair } from '@polkadot/keyring/types'
+import type { DomainInput, NetworkInput } from './network'
+
 export type Mnemonic = {
   mnemonic: string
 }
@@ -13,3 +19,24 @@ export type AppName = {
 }
 
 export type MnemonicOrURI = Mnemonic | URI
+
+export type Wallet = {
+  keyringPair?: KeyringPair
+  injectedAccount?: InjectedAccountWithMeta
+  address: string
+  commonAddress: string
+}
+
+export interface GeneratedWallet extends Wallet {
+  mnemonic: string
+}
+
+export type ActivateWalletInput = (NetworkInput | DomainInput) & MnemonicOrURI & AppName
+
+export type WalletActivated = {
+  api: ApiPromise
+  accounts: InjectedAccountWithMeta[] & KeyringPair[]
+  address: string
+}
+
+export type { ApiPromise, InjectedAccountWithMeta, KeyringPair, Signer, SignerResult }
