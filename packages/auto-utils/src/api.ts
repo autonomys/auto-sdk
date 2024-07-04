a87479dbcd52eb12973c00c39bff51cf3b6db192
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-07-04 08:11:58 -0400

    fix build

--- packages/auto-utils/src/api.ts
+++ packages/auto-utils/src/api.ts
@@ -1,50 +1,32 @@
-import { ApiPromise as PolkadotApiPromise, WsProvider } from '@polkadot/api'
+import { ApiPromise, WsProvider } from '@polkadot/api'
 import { getNetworkDomainRpcUrls, getNetworkRpcUrls } from './network'
-import type { ApiPromise } from './types/api'
 import type { DomainInput, NetworkInput } from './types/network'
-
-let provider: WsProvider | null = null
-let apiInstance: ApiPromise | null = null
-
-let domainProvider: WsProvider | null = null
-let apiDomainInstance: ApiPromise | null = null
 
 export const activate = async (input?: NetworkInput): Promise<ApiPromise> => {
   // Get the first rpc urls for the network
   const rpcUrl = getNetworkRpcUrls(input)
   // Create the provider
-  provider = new WsProvider(rpcUrl[0])
+  const provider = new WsProvider(rpcUrl[0])
   // Create the API instance
-  apiInstance = await PolkadotApiPromise.create({ provider })
+  const api = await ApiPromise.create({ provider })
+  await api.isReady
 
-  return apiInstance
+  return api
 }
 
 export const activateDomain = async (input: DomainInput): Promise<ApiPromise> => {
   // Get the first rpc urls for the network
   const rpcUrl = getNetworkDomainRpcUrls(input)
   // Create the provider
-  domainProvider = new WsProvider(rpcUrl[0])
+  const provider = new WsProvider(rpcUrl[0])
   // Create the API instance
-  apiDomainInstance = await PolkadotApiPromise.create({ provider: domainProvider })
+  const api = await ApiPromise.create({ provider })
+  await api.isReady
 
-  return apiDomainInstance
+  return api
 }
 
-export const disconnect = async (): Promise<void> => {
-  // Disconnect the API instance and the provider if they exist
-  if (apiInstance) {
-    await apiInstance.disconnect()
-    apiInstance = null
-    provider = null
-  }
-}
-
-export const disconnectDomain = async (): Promise<void> => {
-  // Disconnect the API instance and the provider if they exist
-  if (apiDomainInstance) {
-    await apiDomainInstance.disconnect()
-    apiDomainInstance = null
-    domainProvider = null
-  }
+export const disconnect = async (api: ApiPromise): Promise<void> => {
+  // Disconnect the API instance and the provider
+  await api.disconnect()
 }
