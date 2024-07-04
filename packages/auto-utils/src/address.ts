0d428cf2e3bdb11a9e5b1a08fb0138c0a8847b68
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-07-04 08:29:13 -0400

    move ss58 format value in a constant DEFAULT_SS58_FORMAT

--- packages/auto-utils/src/address.ts
+++ packages/auto-utils/src/address.ts
@@ -1,7 +1,9 @@
 // file: src/address.ts
 
 import { decodeAddress, encodeAddress } from '@polkadot/keyring'
+import { DEFAULT_SS58_FORMAT } from './constants/wallet'
 
-export const address = (address: string | Uint8Array): string => encodeAddress(address, 2254)
+export const address = (address: string | Uint8Array): string =>
+  encodeAddress(address, DEFAULT_SS58_FORMAT)
 
 export const decode = (address: string): Uint8Array => decodeAddress(address)
