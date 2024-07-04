2aa6067b98af21240237eec72ec3221836eb3fe4
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-07-04 08:08:53 -0400

    Export more types #60

---
+++ packages/auto-utils/src/types/extrinsic.ts
@@ -0,0 +1,7 @@
+// file: src/types/extrinsic.ts
+
+import type { SubmittableExtrinsic, SubmittableModuleExtrinsics } from '@polkadot/api/types'
+import type { Hash } from '@polkadot/types/interfaces'
+import type { ISubmittableResult } from '@polkadot/types/types'
+
+export type { Hash, ISubmittableResult, SubmittableExtrinsic, SubmittableModuleExtrinsics }
--- packages/auto-utils/src/types/index.ts
+++ packages/auto-utils/src/types/index.ts
@@ -1,3 +1,3 @@
-export * from './api'
+export * from './extrinsic'
 export * from './network'
 export * from './wallet'
