1b70200741eea25eb4e1caeba39422c90ed25815
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-07-10 22:18:25 -0400

    improve api code and hide init warning

--- packages/auto-utils/src/api.ts
+++ packages/auto-utils/src/api.ts
@@ -1,29 +1,40 @@
 import { ApiPromise, WsProvider } from '@polkadot/api'
 import { getNetworkDomainRpcUrls, getNetworkRpcUrls } from './network'
-import type { DomainInput, NetworkInput } from './types/network'
+import type { ActivateInput, ApiOptions, DomainInput, NetworkInput } from './types/network'
 
-export const activate = async (input?: NetworkInput): Promise<ApiPromise> => {
-  // Get the first rpc urls for the network
-  const rpcUrl = getNetworkRpcUrls(input)
+export const createConnection = async (
+  endpoint: string,
+  options?: ApiOptions,
+): Promise<ApiPromise> => {
   // Create the provider
-  const provider = new WsProvider(rpcUrl[0])
+  const provider = new WsProvider(endpoint)
   // Create the API instance
-  const api = await ApiPromise.create({ provider })
+  const api = await ApiPromise.create({
+    ...options,
+    noInitWarn: options?.noInitWarn ?? true,
+    provider,
+  })
   await api.isReady
 
   return api
 }
 
-export const activateDomain = async (input: DomainInput): Promise<ApiPromise> => {
+export const activate = async (input?: ActivateInput<NetworkInput>): Promise<ApiPromise> => {
   // Get the first rpc urls for the network
-  const rpcUrl = getNetworkDomainRpcUrls(input)
-  // Create the provider
-  const provider = new WsProvider(rpcUrl[0])
-  // Create the API instance
-  const api = await ApiPromise.create({ provider })
-  await api.isReady
+  const endpoint = getNetworkRpcUrls(input)
+  // Remove the networkId from the input
+  if (input) delete input.networkId
 
-  return api
+  return await createConnection(endpoint[0], input)
+}
+
+export const activateDomain = async (input: ActivateInput<DomainInput>): Promise<ApiPromise> => {
+  // Get the first rpc urls for the network
+  const endpoint = getNetworkDomainRpcUrls(input)
+  // Remove the domainId from the input
+  const { domainId, ...rest } = input
+
+  return await createConnection(endpoint[0], rest)
 }
 
 export const disconnect = async (api: ApiPromise): Promise<void> => {
