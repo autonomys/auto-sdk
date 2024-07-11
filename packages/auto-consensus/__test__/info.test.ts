4593089e979edc01d1244ae6e75ca4752fe71c3a
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-07-10 23:09:08 -0400

    add block number and block hash test

--- packages/auto-consensus/__test__/info.test.ts
+++ packages/auto-consensus/__test__/info.test.ts
@@ -1,4 +1,4 @@
-import { networkTimestamp } from '@autonomys/auto-consensus'
+import { blockHash, blockNumber, networkTimestamp } from '@autonomys/auto-consensus'
 import { setup } from './helpers'
 
 describe('Verify info functions', () => {
@@ -11,5 +11,19 @@ describe('Verify info functions', () => {
     const timestamp = BigInt(rawTimestamp.toString())
     // Check if the issuance is greater than zero
     expect(timestamp).toBeGreaterThan(BigInt(0))
+  }, 15000)
+
+  test('Check blockNumber return a number greater than zero', async () => {
+    expect(await blockNumber()).toBeGreaterThan(BigInt(0))
+  }, 15000)
+
+  test('Check blockNumber return a number greater than zero', async () => {
+    expect(await blockNumber()).toBeGreaterThan(BigInt(0))
+  }, 15000)
+
+  test('Check blockHash return a hex greater than 0x', async () => {
+    const _blockHash = await blockHash()
+    expect(_blockHash).toMatch(/^0x/)
+    expect(_blockHash.length).toBeGreaterThan(2)
   }, 15000)
 })
