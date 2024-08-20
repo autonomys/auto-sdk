e1e516d8b4a3931cb04044dbcd74e33bc18852c1
Carlos Lostao <hastro1990@gmail.com>
2024-08-20 11:37:36 +0200

    fix: crypto module not initialized correctly

--- packages/auto-id/src/auth.ts
+++ packages/auto-id/src/auth.ts
@@ -1,5 +1,6 @@
 import { ApiPromise } from '@autonomys/auto-utils'
 import { getCertificateSubjectPublicKey } from './registry'
+import { crypto } from './utils/crypto'
 
 //// This function is used to authenticate an Auto-ID user.
 /// It verifies the signature of the challenge using the public key of the Auto-ID user.
