7a1b04304e5dd936085817baadd40fd64fe44bf3
Carlos Lostao <hastro1990@gmail.com>
2024-09-10 17:35:01 +0200

    feat: unwrap multihash to simple blake3 hash

--- packages/auto-drive/src/cid/index.ts
+++ packages/auto-drive/src/cid/index.ts
@@ -1,6 +1,6 @@
 import { encode, PBNode } from '@ipld/dag-pb'
 import { hash } from 'blake3'
-import { CID } from 'multiformats'
+import { CID } from 'multiformats/cid'
 import * as base32 from 'multiformats/bases/base32'
 import * as raw from 'multiformats/codecs/raw'
 import { create } from 'multiformats/hashes/digest'
@@ -22,3 +22,5 @@ export const stringToCid = (str: string) => {
 export const cidFromBlakeHash = (hash: Buffer) => {
   return CID.create(1, raw.code, create(BLAKE3_CODE, hash))
 }
+
+export const blake3HashFromCid = (cid: CID) => cid.multihash.digest
