5092f3d5994868d195740c0ace46c3f58aeb6f18
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-10-11 14:51:52 -0400

    add some basic string utility functions

---
+++ packages/auto-utils/src/string.ts
@@ -0,0 +1,20 @@
+// file: src/string.ts
+
+export const stringify = (value: any) =>
+  JSON.stringify(value, (_, value) => (typeof value === 'bigint' ? value.toString() : value))
+
+export const shortString = (value: string, initialLength = 6, endLength = -4): string =>
+  `${value.slice(0, initialLength)}...${value.slice(endLength)}`
+
+export const capitalizeFirstLetter = (string: string) =>
+  string ? string.charAt(0).toUpperCase() + string.slice(1) : ''
+
+export const fixLengthEntryId = (blockHeight: bigint, indexInBlock?: bigint): string => {
+  const totalLength = 32
+  const str1 = blockHeight.toString().padStart(totalLength, '0')
+
+  if (indexInBlock === undefined) return str1
+
+  const str2 = indexInBlock.toString().padStart(totalLength, '0')
+  return `${str1}-${str2}`
+}
