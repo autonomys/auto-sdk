7ec20cda3f52a68d807475768b7cb01c41136ab2
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-10-13 07:46:23 -0400

    add formatSpacePledged function in auto-utils

--- packages/auto-utils/src/number.ts
+++ packages/auto-utils/src/number.ts
@@ -1,3 +1,4 @@
+import { BIGINT_ZERO } from './constants/number'
 import { DEFAULT_TOKEN_DECIMALS } from './constants/token'
 
 export const parseTokenAmount = (
@@ -14,4 +15,16 @@ export const formatTokenAmount = (
 ) => {
   if (typeof amount === 'bigint') return amount * BigInt(Math.pow(10, decimals))
   return amount * Math.pow(10, decimals)
+}
+
+export const formatSpacePledged = (value: bigint, decimals = 2) => {
+  if (typeof value !== 'bigint' || value === BIGINT_ZERO) return '0 Bytes'
+
+  const k = 1024
+  const dm = decimals < 0 ? 0 : decimals
+  const sizes = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+
+  const i = Math.floor(Math.log(Number(value)) / Math.log(k))
+
+  return (Number(value) / Math.pow(k, i)).toFixed(dm) + ' ' + sizes[i]
 }
