6d360d6e696b90879679b473c08f9b6c42268e1d
Carlos Lostao <hastro1990@gmail.com>
2024-11-03 20:02:33 +0100

    feat: simple circular tests

---
+++ packages/auto-dag-data/tests/compression.spec.ts
@@ -0,0 +1,58 @@
+import { compressFile, COMPRESSION_CHUNK_SIZE, decompressFile } from '../src'
+
+describe('compression', () => {
+  it('compresses and decompresses a file with default options', async () => {
+    const file = Buffer.from('hello'.repeat(1000))
+
+    const compressed = compressFile(
+      (async function* () {
+        yield file
+      })(),
+      {
+        algorithm: 'zlib',
+        level: 9,
+      },
+    )
+
+    const decompressed = decompressFile(compressed, {
+      algorithm: 'zlib',
+      level: 9,
+    })
+
+    let decompressedBuffer = Buffer.alloc(0)
+    for await (const chunk of decompressed) {
+      decompressedBuffer = Buffer.concat([decompressedBuffer, chunk])
+    }
+
+    expect(decompressedBuffer.toString()).toBe(file.toString())
+  })
+
+  it('compresses and decompresses a file with custom chunk size', async () => {
+    const file = Buffer.from('hello'.repeat(1000))
+    const chunkSize = 100
+
+    const compressed = compressFile(
+      (async function* () {
+        yield file
+      })(),
+      {
+        algorithm: 'zlib',
+        level: 9,
+        chunkSize,
+      },
+    )
+
+    const decompressed = decompressFile(compressed, {
+      algorithm: 'zlib',
+      level: 9,
+      chunkSize,
+    })
+
+    let decompressedBuffer = Buffer.alloc(0)
+    for await (const chunk of decompressed) {
+      decompressedBuffer = Buffer.concat([decompressedBuffer, chunk])
+    }
+
+    expect(decompressedBuffer.toString()).toBe(file.toString())
+  })
+})
---
+++ packages/auto-dag-data/tests/encryption.spec.ts
@@ -0,0 +1,76 @@
+import { decryptFile, encryptFile } from '../src'
+import { EncryptorAlgorithm } from '../src/encryption/types'
+
+describe('encryption', () => {
+  it('encrypts and decrypts a file with default chunk size', async () => {
+    const chunk = 'hello'
+    const file = Buffer.from(chunk.repeat(1000))
+    const password = 'password'
+    const salt = 'salt'
+
+    const encrypted = encryptFile(
+      (async function* () {
+        yield file
+      })(),
+      { password, salt },
+      {
+        algorithm: EncryptorAlgorithm.AES_GCM,
+      },
+    )
+
+    const decrypted = decryptFile(
+      encrypted,
+      { password, salt },
+      {
+        algorithm: EncryptorAlgorithm.AES_GCM,
+      },
+    )
+
+    let decryptedBuffer = Buffer.alloc(0)
+    for await (const chunk of decrypted) {
+      decryptedBuffer = Buffer.concat([decryptedBuffer, chunk])
+    }
+
+    expect(decryptedBuffer.toString()).toBe(file.toString())
+  })
+
+  it('encrypts and decrypts a file with custom chunk size', async () => {
+    const chunk = 'hello'
+    const file = Buffer.from(chunk.repeat(1000))
+    const password = 'password'
+    const salt = 'salt'
+    const IV_SIZE = 16
+    const TAG_SIZE = 16
+
+    const encryptingSize = 100
+
+    const encrypted = encryptFile(
+      (async function* () {
+        yield file
+      })(),
+      { password, salt },
+      {
+        algorithm: EncryptorAlgorithm.AES_GCM,
+        chunkSize: encryptingSize,
+      },
+    )
+
+    const decryptingSize = encryptingSize + IV_SIZE + TAG_SIZE
+
+    const decrypted = decryptFile(
+      encrypted,
+      { password, salt },
+      {
+        algorithm: EncryptorAlgorithm.AES_GCM,
+        chunkSize: decryptingSize,
+      },
+    )
+
+    let decryptedBuffer = Buffer.alloc(0)
+    for await (const chunk of decrypted) {
+      decryptedBuffer = Buffer.concat([decryptedBuffer, chunk])
+    }
+
+    expect(decryptedBuffer.toString()).toBe(file.toString())
+  })
+})
