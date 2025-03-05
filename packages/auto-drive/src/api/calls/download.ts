cddf8ac4b490cfe3d8e919b7e97f7c1b69b5c2c7
Carlos Lostao <hastro1990@gmail.com>
2025-03-05 13:49:58 -0500

    feat: manage optional encoding

--- packages/auto-drive/src/api/calls/download.ts
+++ packages/auto-drive/src/api/calls/download.ts
@@ -3,11 +3,16 @@ import { AutoDriveApiHandler } from '../types'
 
 export const downloadObject = async (
   api: AutoDriveApiHandler,
-  query: ArgsWithoutPagination<{ cid: string }>,
+  query: ArgsWithoutPagination<{ cid: string; ignoreBackendEncoding?: boolean }>,
 ): Promise<ReadableStream<Uint8Array>> => {
-  const response = await api.sendRequest(`/objects/${query.cid}/download`, {
-    method: 'GET',
-  })
+  const ignoreBackendEncoding = query.ignoreBackendEncoding ?? true
+
+  const response = await api.sendRequest(
+    `/objects/${query.cid}/download?ignoreEncoding=${ignoreBackendEncoding}`,
+    {
+      method: 'GET',
+    },
+  )
 
   if (!response.ok) {
     throw new Error(`Failed to download file: ${response.statusText}`)
