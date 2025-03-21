9dd9ef0b70a70f9a53356d08e5e6425f9b8acf9e
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2025-03-21 15:23:04 -0400

    add more xdm function

--- packages/auto-xdm/src/info.ts
+++ packages/auto-xdm/src/info.ts
@@ -1,11 +1,19 @@
 // file: src/types/index.ts
 
 import { query } from '@autonomys/auto-consensus'
-import type { ApiPromise, Codec } from '@autonomys/auto-utils'
+import { createDomainsChainIdType, type ApiPromise, type Codec } from '@autonomys/auto-utils'
 
-export const chainAllowlist = async (api: ApiPromise) => {
-  return await query<Codec>(api, 'messenger.chainAllowlist', [])
-}
+export const chainAllowlist = async (api: ApiPromise) =>
+  await query<Codec>(api, 'messenger.chainAllowlist', [])
+
+export const channels = async (api: ApiPromise, chainId: Codec) =>
+  await query<Codec>(api, 'messenger.channels', [chainId])
+
+export const consensusChannels = async (api: ApiPromise) =>
+  await query<Codec>(api, 'messenger.channels', [createDomainsChainIdType(api)])
+
+export const domainChannels = async (api: ApiPromise, domainId: number) =>
+  await query<Codec>(api, 'messenger.channels', [createDomainsChainIdType(api, domainId)])
 
 export const allCancelledTransfers = async (api: ApiPromise) => {
   return await query<Codec>(api, 'transporter.cancelledTransfers', [])
