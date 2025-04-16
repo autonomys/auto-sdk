a80bde57d643cdec925542c086f41887fad81ad2
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2025-04-16 08:56:23 -0400

    fix userSessionCIDFromHash()

--- packages/utility/user-session/src/utils/hash.ts
+++ packages/utility/user-session/src/utils/hash.ts
@@ -8,7 +8,12 @@ import { hexlify, keccak256, toUtf8Bytes, zeroPadValue } from 'ethers'
 
 export const userIdHash = (userId: string) => keccak256(toUtf8Bytes(userId))
 
-export const userSessionCIDHash = (CID: string) =>
-  zeroPadValue(hexlify(blake3HashFromCid(stringToCid(CID))), 32)
+export const userSessionCIDHash = (CID: string) => {
+  const blake3Hash = blake3HashFromCid(stringToCid(CID))
+  return zeroPadValue(hexlify(blake3Hash), 32)
+}
 
-export const userSessionCIDFromHash = (hash: Buffer) => cidToString(cidFromBlakeHash(hash))
+export const userSessionCIDFromHash = (hash: string) => {
+  const hashBuffer = Buffer.from(hash.slice(2), 'hex')
+  return cidToString(cidFromBlakeHash(hashBuffer))
+}
