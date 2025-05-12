6094db5885aedcd6443433e6c00202079d3d4134
Naman Anand <iamnamananand996@gmail.com>
2025-05-12 23:41:14 +0530

    fix: 🐛 add support for ES module

--- scripts/generate-pr-changelog.js
+++ scripts/generate-pr-changelog.js
@@ -8,7 +8,13 @@
  * export GITHUB_TOKEN=your_github_personal_access_token
  */
 
-const { Octokit } = require('@octokit/rest')
+// Replace require with dynamic import at the top level
+let Octokit
+import('@octokit/rest').then((module) => {
+  Octokit = module.Octokit
+  updateChangelog()
+})
+
 const fs = require('fs')
 const path = require('path')
 const { execSync } = require('child_process')
@@ -58,10 +64,8 @@ if (!process.env.GITHUB_TOKEN) {
   process.exit(1)
 }
 
-// Create Octokit instance with auth token
-const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN,
-})
+// Create Octokit instance with auth token - moved to updateChangelog function
+let octokit
 
 /**
  * Get all tags sorted by date
@@ -288,6 +292,10 @@ async function generateChangelogBetweenTags(startTag, endTag, version) {
  */
 async function updateChangelog() {
   try {
+    octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN,
+    })
+
     // Get all tags
     console.log('Fetching tags...')
     const tags = await getTags()
@@ -401,5 +409,4 @@ async function updateChangelog() {
   }
 }
 
-// Run the script
-updateChangelog()
+// Don't immediately call updateChangelog() here anymore since we call it after importing Octokit
