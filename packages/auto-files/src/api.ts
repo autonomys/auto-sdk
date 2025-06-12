bcea53608c32f4cc64e04a546f29a566a6551579
Carlos Lostao <hastro1990@gmail.com>
2025-06-12 19:27:42 +0200

    feat: create chunked partial downloading from files gateway

---
+++ packages/auto-files/src/api.ts
@@ -0,0 +1,71 @@
+import { Readable } from 'stream'
+import { withRetries } from './utils'
+
+interface FetchedFile {
+  length: number
+  encoding: string
+  data: Readable
+}
+
+export const createAutoFilesApi = (baseUrl: string, apiSecret: string) => {
+  const authFetch = (url: string, options: RequestInit = {}) => {
+    const urlObj = new URL(url)
+    urlObj.searchParams.set('api_key', apiSecret)
+    console.log('Fetching', urlObj.toString())
+    return fetch(urlObj.toString(), {
+      ...options,
+      headers: { ...options.headers },
+    })
+  }
+
+  const getChunk = async (cid: string, chunk: number): Promise<ArrayBuffer | null> => {
+    const response = await authFetch(`${baseUrl}/files/${cid}/partial?chunk=${chunk}`)
+    if (!response.ok) {
+      throw new Error('Error fetching chunk')
+    }
+
+    if (response.status === 204) {
+      return null
+    }
+
+    const buffer = await response.arrayBuffer()
+    console.log('Chunk download finished:', buffer.byteLength)
+    return buffer
+  }
+
+  const getFile = async (
+    cid: string,
+    {
+      retriesPerFetch = 3,
+      onProgress,
+    }: { retriesPerFetch?: number; onProgress?: (progress: number) => void } = {},
+  ): Promise<FetchedFile> => {
+    const response = await withRetries(
+      () => authFetch(`${baseUrl}/files/${cid}/info`),
+      retriesPerFetch,
+    )
+    if (!response.ok) {
+      throw new Error('Error fetching file header')
+    }
+
+    const length = parseInt(response.headers.get('Content-Length') ?? '0')
+    const encoding = response.headers.get('Content-Encoding') ?? 'utf-8'
+
+    let i = 0
+    let totalDownloaded = 0
+    return {
+      data: new Readable({
+        async read() {
+          const chunk = await withRetries(() => getChunk(cid, i++), retriesPerFetch)
+          this.push(chunk ? Buffer.from(chunk) : null)
+          totalDownloaded += chunk?.byteLength ?? 0
+          onProgress?.(totalDownloaded / length)
+        },
+      }),
+      length,
+      encoding,
+    }
+  }
+
+  return { getFile }
+}
