a17fac2f92b1dc61fe4ba484554476c0dcd33897
clostao <49268919+clostao@users.noreply.github.com>
2025-07-15 09:22:38 +0200

    feat: add input sanitization for FilePreview (#447)
    * chore: add dompurify dependency to auto-design-system
    
    * feat: add sanitizeHTML utility using DOMPurify for safe HTML rendering
    
    * feat: integrate sanitizeHTML utility into FilePreview for safe text and file name rendering
    
    * feat: integrate sanitizeHTML utility into ImagePreview
    
    * refactor: simplify sanitizedFileName computation in FilePreview component

--- packages/auto-design-system/src/components/common/FilePreview/ImageViewer.tsx
+++ packages/auto-design-system/src/components/common/FilePreview/ImageViewer.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { cn } from '../../../utils/cn'
+import { sanitizeHTML } from '../../../utils/sanitizeHTML'
 
 export interface ImageViewerProps {
   src: string
@@ -7,11 +8,15 @@ export interface ImageViewerProps {
 }
 
 export const ImageViewer = ({ src, alt }: ImageViewerProps) => {
+  const sanitizedAlt = useMemo(() => {
+    return alt ? sanitizeHTML(alt) : null
+  }, [alt])
+
   return (
     <div className='relative flex flex-col items-center'>
       <img
         src={src}
-        alt={alt || 'Image preview'}
+        alt={sanitizedAlt || 'Image preview'}
         className={cn(
           'max-h-[50vh] w-auto object-contain dark:border dark:border-gray-700 dark:bg-gray-900',
         )}
